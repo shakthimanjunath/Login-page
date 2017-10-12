6a6c76b13fa8be7e8d5139740d6cfe1d0fc27f75
shakthimanjunath <shakthi@geekyants.com>
2017-10-12 14:11:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import React from "react";
 import Login from "./login";
-import { Router, Route, Link, IndexRoute } from "react-router";
+
 
 export class App extends React.Component {
     render() {
-        return (
-            (
-                <Router>
-                    <Route path="/" component={App} />
-                    {/* add the routes here */}
-
-                    <Route exact path="/src/login" component={Login} />
-                </Router>
-            ),
-            document.getElementById("app")
-        );
+       <Login/>
     }
 }
